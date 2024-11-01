0a5f0d0d5457109fe2078f52fe6ba3095eb80b78
kabilanmohan <kabilanmohans@gmail.com>
2024-11-01 22:00:50 +0530

    Problem Difficulty Level : Medium(But actually Hard)

---
+++ Dynamic Programming/1696. Jump Game VI.cpp
@@ -0,0 +1,37 @@
+
+    /*
+    Time complexity : O(n*k)
+    Space complexity : O(n)
+    */
+
+
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        vector<int> dp(nums.size() , -1);
+        return func_result(0 , nums , k , dp);
+    }
+
+    int func_result(int idx , vector<int>& nums, int k , vector<int>& dp)
+    {
+        if(idx == nums.size()-1) return nums[idx];
+
+        if(dp[idx] != -1) return dp[idx];
+        int max_sum = -1e9;
+
+        for(int i = 1 ; i <= k  && idx+i < nums.size(); i++)
+        {
+            max_sum = max(max_sum , (nums[idx] + func_result(idx + i , nums , k , dp)));
+        }
+
+        return dp[idx] = max_sum;
+    }
+};
+
+
+    /*
+    Author : kabilanmohan
+    Question Link : https://leetcode.com/problems/jump-game-vi/
+    */
+
+    
