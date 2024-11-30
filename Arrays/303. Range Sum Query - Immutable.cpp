8aa24466f03a7e0c16cffb7bea955d7ad2393e82
kabilanmohan <kabilanmohans@gmail.com>
2024-11-30 15:11:36 +0530

    Problem Difficulty Level : Easy

---
+++ Arrays/303. Range Sum Query - Immutable.cpp
@@ -0,0 +1,40 @@
+
+    /*
+    Time complexity : O(n)
+    Space complexity : O(n)
+    */
+
+
+class NumArray {
+public:
+    vector<int> prefix;
+    NumArray(vector<int>& nums) 
+    {
+        int sum = 0;
+        for(int i = 0 ; i < nums.size() ; i++)
+        {
+            sum += nums[i];
+            prefix.push_back(sum);
+        }
+    }
+    
+    int sumRange(int left, int right) 
+    {
+        if(left == 0) return prefix[right];
+        return prefix[right] - prefix[left - 1];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
+
+
+    /*
+    Author : kabilanmohan
+    Question Link : https://leetcode.com/problems/range-sum-query-immutable/
+    */
+
+    
